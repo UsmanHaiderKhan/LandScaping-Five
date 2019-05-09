19ea011147c53bbe7876c2309e84576e7fe45d27
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2019-05-09 13:51:40 +0500

    Update setup the boostrap 4 environment

---
+++ LandScaping-Five/assets/Bootstrap4/js/script.js
@@ -0,0 +1,192 @@
+﻿//console.log("usman");
+
+/*================== Read More Text ==================*/
+
+$(function () {
+    var showChar = 490;
+    var moretext = "Read More >>";
+    var lesstext = "Read Less <<";
+    $('.comments-space').each(function () {
+        var content = $(this).html();
+        if (content.length > showChar) {
+            var show_content = content.substr(0, showChar);
+            var hide_content = content.substr(showChar, content.length - showChar);
+            var html = show_content +
+                '<span class="remaining-contents"><span class=" p-3">' +
+                hide_content +
+                '</span>' +
+
+                '<a href="" class="morelinksss read-text " style="display:block;">' +
+                moretext +
+                '</a>' + '</span>';;
+
+            $(this).html(html);
+        }
+    });
+
+    $(".morelinksss").click(function () {
+
+        if ($(this).hasClass("less")) {
+            $(this).removeClass("less");
+            $(this).html(moretext);
+        } else {
+            $(this).addClass("less");
+            $(this).html(lesstext);
+        }
+        $(this).parent().prev().toggle();
+        $(this).prev().toggle();
+        return false;
+    });
+});
+$(function () {
+    var showChar = 280;
+    var moretext = " Read More >>";
+    var lesstext = "Read Less <<";
+    $('.comments-spaces').each(function () {
+        var content = $(this).html();
+        if (content.length > showChar) {
+            var show_content = content.substr(0, showChar);
+            var hide_content = content.substr(showChar, content.length - showChar);
+            var html = show_content + '<span class="remaining-contents"><span>' + hide_content + '</span>' +
+
+                '<a href="" class="morelinks read-texts mt-10" style="display:block;">' + moretext + '</a>'
+                + '</span>';
+            $(this).html(html);
+        }
+    });
+
+    $(".morelinks").click(function () {
+
+        if ($(this).hasClass("less")) {
+            $(this).removeClass("less");
+            $(this).html(moretext);
+        } else {
+            $(this).addClass("less");
+            $(this).html(lesstext);
+        }
+        $(this).parent().prev().toggle();
+        $(this).prev().toggle();
+        return false;
+    });
+});
+
+/*===================== Smooth Scrolling ======================*/
+$(function () {
+    $('a').smoothScroll();
+});
+/*======================= Nav Active Class =======================*/
+
+$(function () {
+    $('.nav-item').on('click',
+        function () {
+            $('.nav-item').removeClass('active');
+            $(this).addClass('active');
+        });
+});
+
+/*===================== Load More Images ======================*/
+$(document).ready(function () {
+
+    $('.loadMore').loadMoreResults({
+        displayedItems: 12,
+        showItems: 4
+    });
+
+
+});
+
+/*===================== Owl Carousel Slider ======================*/
+$(function () {
+    $('.owl-carousel').owlCarousel({
+        loop: false,
+        margin: 10,
+        dots: true,
+        nav: false,
+        slideBy: 3,
+        item: 3,
+
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1,
+                dots: true,
+                nav: false
+            },
+            400: {
+                items: 1,
+                dots: true,
+                nav: false
+            },
+            600: {
+                items: 1,
+                dots: true,
+                nav: false
+            },
+            768: {
+                items: 2,
+                dots: true,
+                nav: false
+            },
+            800: {
+                items: 2,
+                dots: true,
+                nav: false
+            },
+            1000: {
+                items: 3,
+                dots: true,
+                nav: false
+            }
+        }
+    });
+});
+
+/*===================== Scroll Top Function Script ======================*/
+
+
+$(function () {
+    $(document).scroll(function () {
+        var $nav = $(".navbar");
+        $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+    });
+    $(document).scroll(function () {
+        var $nav = $(".nav-link");
+        $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+    });
+    $(document).scroll(function () {
+        var $nav = $(".active");
+        $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+    });
+});
+/*===================== Nav tabs active Function Script ======================*/
+
+/*===================== Another Load More Script ======================*/
+$(function () {
+    $(".no-display").slice(0, 3).show();
+    $("#load-more").on('click', function (e) {
+        e.preventDefault();
+        $(".no-display:hidden").slice(0, 3).slideDown();
+        if ($(".no-display:hidden").length == 0) {
+            $("#load-more").fadeOut('slow');
+
+
+        }
+        $('html,body').animate({
+            scrollTop: $(this).offset().center
+        }, 1500);
+    });
+});
+
+
+
+
+
+
+/*=====================Active Class Script ======================*/
+$(function () {
+    $('.form-check-label').on('click',
+        function () {
+            $('.form-check-label').removeClass('radio-active');
+            $(this).addClass('radio-active');
+        });
+});
